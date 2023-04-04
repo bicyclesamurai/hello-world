ae476f52b01b178f06b42e513ed567a2cc869f12
bicyclesamurai <129838905+bicyclesamurai@users.noreply.github.com>
2023-04-04 21:31:24 +0900

    uploaded code

---
+++ helloWorld/index.php
@@ -0,0 +1,26 @@
+<?php
+	session_start();
+	echo"<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'>";
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
+echo "<html>";
+echo "<head>";
+	echo "<meta http-equiv='content-type' content='text/html;charset=ISO-8859-1'>";
+	echo "<title>Hello World</title>";
+	echo "<link rel='StyleSheet'' href='/navigator/css/main.css' type='text/css'>";
+echo "</head>";
+
+echo "<body>";
+
+	
+ 	echo "<center><br><br><br><br><br>";
+    echo "Chubby Chicken says...<BR>";
+    echo "<H1>Hello World</H1>";
+ 	echo "<img src='images/chubbychicken.png' alt='Chubby Chicken Mascot'><br>\n";
+
+	?>
+</body>
+
+</html>
+
